c14def77e4bb10ce774f0d6a164c306b30c102ed
MGoldobin <misha2000goldobin@mail.ru>
2021-10-24 23:13:10 +0500

    create App component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Content from '../Content/Content'
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="app__title">Event Listing</h1>
+      <Content />
+    </div>
+  )
+}
+
+export default App
