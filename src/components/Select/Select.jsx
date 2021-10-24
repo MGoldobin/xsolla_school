e1094fd4f635e064cfc444d04d076066799001b1
MGoldobin <misha2000goldobin@mail.ru>
2021-10-24 23:14:00 +0500

    create Select component

---
+++ src/components/Select/Select.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import './Select.css'
+import { filterCity, filterMonth, filterAll } from '../../vendor/functions'
+
+const Select = ({ options, def, id, setFilter, filter, apiData }) => {
+	useEffect(() => {
+		let arr = apiData
+		if (filter.month !== "" && filter.month !== "0") arr = filterMonth(apiData, filter.month)
+		if ((filter.month === "" || filter.month === "0") && filter.city !== "") arr = filterCity(apiData, filter.city)
+		if ((filter.month !== "" && filter.month !== "0") && filter.city !== "") arr = filterAll(apiData, filter.city, filter.month)
+		if ((filter.month === "" || filter.month === "0") && filter.city === "") arr = apiData
+		setFilter(prev => ({ ...prev, data: arr }))
+		console.log(filter)
+	}, [filter.city, filter.month])
+
+	return (
+		<select className="select" id={id} onChange={(e) => {
+			const city = id === "city" ? e.target.value : filter.city
+			const month = id === "month" ? e.target.selectedIndex : filter.month
+			setFilter({ data: filter.data, city: city, month: String(month) })
+		}
+		}>
+			<option className="select__option" value={def}>{def}</option>
+			{
+				options.map(option => (<option value={option} key={option}>{option}</option>))
+			}
+		</select>
+	)
+}
+
+export default Select
