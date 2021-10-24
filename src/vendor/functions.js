d49f4031f81f1f65bffa94e8fb8170574d21ab87
MGoldobin <misha2000goldobin@mail.ru>
2021-10-24 23:14:27 +0500

    add functions

---
+++ src/vendor/functions.js
@@ -0,0 +1,31 @@
+export function uniqueArr(arr) {
+	let result = []
+
+	for (let str of arr) {
+		if (!result.includes(str)) {
+			result.push(str)
+		}
+	}
+
+	return result
+}
+
+export function getDay(date) {
+	return String(date).slice(0, 2)
+}
+
+export function getMonth(date) {
+	return (String(date)[3] === '0') ? String(date).slice(4, 5) : String(date).slice(3, 5)
+}
+
+export function filterCity(arr, value) {
+	return arr.filter(item => item.city === value)
+}
+
+export function filterMonth(arr, value) {
+	return arr.filter(item => getMonth(item.date) === value)
+}
+
+export function filterAll(arr, city, month) {
+	return arr.filter(item => getMonth(item.date) === month && item.city === city)
+}
