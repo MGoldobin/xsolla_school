671e22aba8645ae57fd11faa1dd88ed12787c027
MGoldobin <misha2000goldobin@mail.ru>
2021-10-26 19:48:03 +0500

    Update Select.jsx

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,22 +1,21 @@
 import React, { useEffect } from 'react'
 import './Select.css'
-import { filterCity, filterMonth, filterAll } from '../../vendor/functions'
+import { filterCity, filterMonth, filterFavorite } from '../../vendor/functions'
 
-const Select = ({ options, def, id, setFilter, filter, apiData }) => {
+const Select = ({ options, def, id, setFilter, filter, apiData, favoriteArr }) => {
 	useEffect(() => {
 		let arr = apiData
-		if (filter.month !== "" && filter.month !== "0") arr = filterMonth(apiData, filter.month)
-		if ((filter.month === "" || filter.month === "0") && filter.city !== "") arr = filterCity(apiData, filter.city)
-		if ((filter.month !== "" && filter.month !== "0") && filter.city !== "") arr = filterAll(apiData, filter.city, filter.month)
-		if ((filter.month === "" || filter.month === "0") && filter.city === "") arr = apiData
+		arr = filter.city ? filterCity(arr, filter.city) : arr
+		arr = (filter.month && filter.month !== "0") ? filterMonth(arr, filter.month) : arr
+		arr = filter.favorite ? filterFavorite(arr, favoriteArr) : arr
 		setFilter(prev => ({ ...prev, data: arr }))
-	}, [filter.city, filter.month])
+	}, [filter.city, filter.month, filter.favorite, favoriteArr])
 
 	return (
 		<select className="select" id={id} onChange={(e) => {
 			const city = id === "city" ? e.target.value : filter.city
 			const month = id === "month" ? e.target.selectedIndex : filter.month
-			setFilter({ data: filter.data, city: city, month: String(month) })
+			setFilter(prev => ({ ...prev, city: city, month: String(month) }))
 		}
 		}>
 			<option className="select__option" value={def}>{def}</option>
